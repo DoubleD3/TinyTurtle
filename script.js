1f930e1044ce5be1bcc58ec159e946147c5b2632
Daniel David <daniel.david@students.ptechnyc.org>
2017-01-11 14:58:14 +0000

    Turtle made a house!

---
+++ script.js
@@ -0,0 +1,40 @@
+  TinyTurtle.apply(window); 
+  
+   forward(25);
+   
+   left(90);
+   
+   forward(50);
+   
+   left(90);
+   
+   forward(25);
+   
+   left(90);
+   
+   forward(50);
+   
+   left(90);
+   
+   forward(25);
+   
+   left(45);
+   
+   forward(35);
+   
+   left(90);
+   
+   forward(35);
+   
+   left(45);
+   
+   forward(25);
+   
+   left(90);
+   
+   forward(25);
+   
+   left(90);
+  
+   stamp();
+  
